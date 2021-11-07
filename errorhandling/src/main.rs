e6e09050b9cd8148bd8561ce3b19a567aad5a246
krishpranav <krisna.pranav@gmail.com>
2021-11-07 16:36:55 +0530

    errorhandling: main errorhandling

--- errorhandling/src/main.rs
+++ errorhandling/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("test.jpg");
+    
+    match f {
+        Ok(f) => {
+            println!("file found {:?}", f);
+        },
+        Err(e) => {
+            println!("file not found \n{:?}", e);
+        }
+    }
 }
