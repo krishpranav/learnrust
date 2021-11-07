a65d600b595e6deac8bfdd5b374acc5c625a1d05
krishpranav <krisna.pranav@gmail.com>
2021-11-07 16:53:19 +0530

    filehandling

--- filehandling/src/main.rs
+++ filehandling/src/main.rs
@@ -1,3 +1,7 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = std::fs::File::create("data.txt").expect("create failed");
+    file.write_all("Hello World".as_bytes()).expect("write failed");
+    println!("file created: data.txt\n");
 }
