7191696dbf5db6b747e95dc94310fb1ba9789560
krishpranav <krisna.pranav@gmail.com>
2021-11-07 12:08:09 +0530

    datatypes
    datatypes + workflow improved

---
+++ datatypes/README.md
@@ -0,0 +1,5 @@
+## Basic DataTypes:
+
+```
+$ cargo run
+```
---
+++ datatypes/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let name = "UserOne";
+    let age:u32 = 20;
+    let company = "CompanyOne Pvt Limited";
+    let isprogrammer = true;
+
+    println!("Name: {}", name);
+    println!("Age: {}", age);
+    println!("Company: {}", company);
+    println!("Is he programmer??: {}", isprogrammer);
+}
